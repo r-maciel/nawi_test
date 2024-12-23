9516264830c1656e8f5bbb98e4ba0b98e28b2cbc
Luis Maciel <lmaciel.amador@gmail.com>
2024-12-23 10:56:21 -0500

    Catch exception for taken username

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+from sqlalchemy.exc import IntegrityError
 from app.db import SessionDep
 from app.models.user import User
 from app.utils.auth import hash_password
+from app.exceptions import UserIntegrityException
 
 router = APIRouter(prefix="/users")
 
@@ -10,7 +12,15 @@ router = APIRouter(prefix="/users")
 def register_user(user: User, session: SessionDep) -> dict:
     user.id = None
     user.password = hash_password(user.password)
-    session.add(user)
-    session.commit()
+
+    try:
+        session.add(user)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        raise UserIntegrityException(
+            status_code=400,
+            detail="Username is already taken."
+        )
 
     return {"message": "User created successfully"}
