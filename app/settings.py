956240ac89793478a3df1873b7a07bf4722434b8
Luis Maciel <lmaciel.amador@gmail.com>
2024-12-23 02:15:36 -0500

    Add Setting class
    Create Setting class to load environment vars from dotenv file

---
+++ app/settings.py
@@ -0,0 +1,22 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+DOTENV_FILENAME = os.path.join(".env")
+
+
+def _get_dotenv_filepath() -> str:
+    current_dir = os.path.abspath(__file__)
+    app_dir = os.path.dirname(current_dir)
+    project_dir = os.path.dirname(app_dir)
+
+    return os.path.join(project_dir, DOTENV_FILENAME)
+
+
+class Settings(BaseSettings):
+    db_url: str
+
+    model_config = SettingsConfigDict(env_file=_get_dotenv_filepath())
+
+
+settings = Settings()
